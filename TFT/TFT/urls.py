768406503be42da19073b0200f5bff26b40d3c82
frutt <jordan5rov@gmail.com>
2022-01-27 03:16:01 +0200

    Added landing page

--- TFT/TFT/urls.py
+++ TFT/TFT/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from landing_page.views import home
 
 urlpatterns = (
+    path('', home),
     path('admin/', admin.site.urls),
     path('champions/', include('TFT.champions.urls')),
 )
